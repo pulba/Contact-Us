9b867c6088523b4b77a6dc869230b699ef495ac3
pulba <pulbagrup@gmail.com>
2024-09-05 22:38:40 +0700

    second

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var dataParts = data.split('|');
     var newDiv = document.createElement('div');
     newDiv.className =
-      'flex flex-col leading-1.5 w-full p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl font-poppins';
+      'flex flex-col leading-1.5 w-full p-4 mb-3 bg-gray-100 rounded-md font-poppins';
     newDiv.innerHTML = `
       <div class="flex items-center justify-between">
         <p class="text-md font-semibold text-gray-800 capitalize">${dataParts[0]}</p>
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     e.preventDefault();
 
     var nameInput = document.getElementById('name');
-    var dateInput = document.getElementById('date'); // Pastikan elemen ini ada jika digunakan
     var messageInput = document.getElementById('message');
 
     if (!nameInput || !messageInput) {
@@ -37,19 +36,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
       return;
     }
 
-    var nameForm = nameInput.value;
-    var dateForm = dateInput ? dateInput.value : ''; // Tangani jika dateInput tidak ada
+    var nameForm = nameInput.value;    
     var radioInput = document.querySelector('input[name="radio"]:checked');
     var radioValue = radioInput ? radioInput.value : '';
     var messageForm = messageInput.value;
 
     if (nameForm === '' && messageForm === '') {
-      // Jika nama dan pesan kosong, tidak menambah data
       console.error('Name or message is empty.');
       return;
     }
 
-    var formattedData = `${nameForm}|${dateForm}|${radioValue}|${messageForm}`;
+    var formattedData = `${nameForm}|${radioValue}|${messageForm}`;
     dataArray.unshift(formattedData);
     localStorage.setItem('dataArray', JSON.stringify(dataArray));
 
@@ -64,11 +61,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
       <p class="text-base italic text-left py-5 text-gray-800/50 font-medium">${messageForm}</p>
     `;
     displayDiv.prepend(newDiv);
-    displayDiv.style.display = 'block'; // Pastikan displayDiv terlihat
+    displayDiv.style.display = 'block'; 
 
-    // Kosongkan input form setelah submit
     nameInput.value = '';
-    if (dateInput) dateInput.value = ''; // Kosongkan jika ada
     var radios = document.querySelectorAll('input[name="radio"]');
     radios.forEach((radio) => {
       radio.checked = false;
@@ -77,11 +72,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 
   clearButton.addEventListener('click', function () {
-    // Menghapus data dari localStorage
     localStorage.removeItem('dataArray');
-    // Menghapus semua elemen dari div display
     displayDiv.innerHTML = '';
-    // Sembunyikan displayDiv jika tidak ada data
     displayDiv.style.display = 'none';
   });
 });
